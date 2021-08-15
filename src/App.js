3db13dd15d4e3884209f51caf52cde590f3e94e6
hitesh-code12 <hitesh.bhonkar98@gmail.com>
2021-08-15 15:12:42 +0530

    carsoal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Matercarosal from './Component/Matercarosal';
 
 function App() {
   return (
     <div className="App"  >
      <h2>sssss</h2>
-     <h1>Hello Hitesh How Are u?</h1>
+     <h1>Hello Hith How Are u?</h1>
+     <Matercarosal></Matercarosal>
     </div>
   );
 }
